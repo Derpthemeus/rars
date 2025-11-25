e56d41e602ce402e15a7db06babc55a81e98a94a
Matthew Oslan <derpthemeus@gmail.com>
2025-11-24 23:24:03 -0500

    Revert "Print stack trace"
    This reverts commit b7e8d684bfb3f043eb109ade50e70d1ba56cfcb1.

--- src/rars/riscv/instructions/Store.java
+++ src/rars/riscv/instructions/Store.java
@@ -56,7 +56,6 @@ public abstract class Store extends BasicInstruction {
         try {
             store(RegisterFile.getValue(operands[2]) + operands[1], RegisterFile.getValueLong(operands[0]));
         } catch (AddressErrorException e) {
-            e.printStackTrace();
             throw new SimulationException(statement, e);
         }
     }
